0fb0c078d92e48806e1d229298a3465266ab258e
Daniela <nani06994tr@gmail.com>
2025-01-02 22:01:32 +0100

    code refactoring

--- game.py
+++ game.py
@@ -9,6 +9,12 @@ import matplotlib.image as mpimg
 import pygame
 import time
 
+# FUNCION PARA VALIDAR ENTRADAS
+def validation(value, min, max):
+    while value < min or value > max:
+        value = int(input(Fore.BLUE + f"\uFE0F TIENES QUE SELECCIONAR UNA OPCIÓN ENTRE {min} Y {max}: "))
+    return value
+
 #FUNCION PARA MOSTRAR EL MENU PRINCIPAL
 def Menu():
     print(Fore.BLUE + "▓" * 50)
@@ -22,6 +28,18 @@ def Menu():
         "\t4. \U0001F6AA SALIR" +
         Style.RESET_ALL
     )
+# FUNCION PARA MOSTRAR SUBMENU Y ELEGIR DIFICULTAD
+def submenu():
+    print(Fore.LIGHTCYAN_EX + "SELECCIONA EL NIVEL DE DIFICULTAD".center(40))
+    print(
+        Fore.LIGHTCYAN_EX + "\t1. \U0001F480 FÁCIL\n" +
+        "\t2. \U0001F480 \U0001F480 MEDIO\n" +
+        "\t3. \U0001F480 \U0001F480 \U0001F480 DIFÍCIL\n" +
+        Style.RESET_ALL
+    )
+    difficulty = int(input(Fore.BLUE + "SELECCIONA EL NIVEL DE DIFICULTAD: "))
+    difficulty = validation(difficulty,1,3)
+    return {1: 20, 2: 12, 3: 5}[difficulty]
 
 # FUNCION PARA REPRODUCIR SONIDO DE GANAR O PERDER
 def animation_game (url_photo, url_sound):
@@ -38,27 +56,9 @@ def animation_game (url_photo, url_sound):
     plt.axis('off')
     plt.show()
 
-# FUNCION PARA VALIDAR ENTRADAS
-def validation(value, min, max):
-    while value < min or value > max:
-        value = int(input(Fore.BLUE + f"UPS! TIENES QUE SELECCIONAR UNA OPCIÓN ENTRE {min} Y {max}: "))
-    return value
-
-# FUNCION PARA MOSTRAR SUBMENU Y ELEGIR DIFICULTAD
-def submenu():
-    print(Fore.LIGHTCYAN_EX + "SELECCIONA EL NIVEL DE DIFICULTAD".center(40))
-    print(
-        Fore.LIGHTCYAN_EX + "\t1. \U0001F480 FÁCIL\n" +
-        "\t2. \U0001F480 \U0001F480 MEDIO\n" +
-        "\t3. \U0001F480 \U0001F480 \U0001F480 DIFÍCIL\n" +
-        Style.RESET_ALL
-    )
-    difficulty = int(input(Fore.BLUE + "SELECCIONA EL NIVEL DE DIFICULTAD: "))
-    difficulty = validation(difficulty,1,3)
-    return {1: 20, 2: 12, 3: 5}[difficulty]
-
 # FUNCION LOGICA DEL JUEGO
-def play_game(max_attempts,unknown_number,name):
+def play_game(unknown_number,name):
+    max_attempts = submenu()
     print(Fore.LIGHTWHITE_EX + f"{name}, TIENES {max_attempts} INTENTOS PARA ADIVINAR!.")
     attempts = 0
     guess_number = 0
@@ -76,7 +76,7 @@ def play_game(max_attempts,unknown_number,name):
             print("EL NÚMERO A ADIVINAR ES MAYOR")
         attempts += 1
         if win == False:
-            print(f'TE QUEDAN {max_attempts - attempts} INTENTOS DISPONIBLES')
+            print(Fore.RED + f'TE QUEDAN {max_attempts - attempts} INTENTOS DISPONIBLES')
     if max_attempts == attempts:
         print(Fore.RED + f"{name} HAS SUPERADO EL NÚMERO MÁXIMO DE INTENTOS PERMITIDOS\nSUERTE PARA LA PRÓXIMA")
         animation_game('lose.jpg','game_over.wav')
@@ -84,25 +84,22 @@ def play_game(max_attempts,unknown_number,name):
 
 # FUNCION PARA MODO SOLITARIO
 def one_player():
-    print(Fore.BLUE + "=" * 45)
-    print(Fore.LIGHTWHITE_EX + "\U0001F3AE PARTIDA MODO SOLITARIO.".center(40))
+    print(Fore.LIGHTWHITE_EX + "=" * 45 + "\n" + "\U0001F3AE PARTIDA MODO SOLITARIO.".center(40))
     print(Fore.LIGHTWHITE_EX + f"DESCUBRE EL NÚMERO QUE HEMOS ESCONDIDO ENTRE 1 Y 1000.")
-    max_attempts = submenu()
     unknown_number = randint(1, 1000)
     print(unknown_number)
-    name = input("INTRODUCE TU NOMBRE O ALIAS: ").lower()
-    play_game(max_attempts, unknown_number,name)
+    name = input("INTRODUCE TU NOMBRE: ").lower()
+    play_game(unknown_number,name)
 
 # FUNCION PARA MODO 2 JUGADORES
 def two_players():
-    print(Fore.BLUE + "=" * 45)
-    print(Fore.LIGHTWHITE_EX + "\U0001F465 PARTIDA 2 JUGADORES.".center(40))
-    max_attempts = submenu()
-    print(Fore.LIGHTWHITE_EX + "JUGADOR 1: ADIVINARÁS UN NÚMERO ENTRE 1 Y 1000 QUE EL JUGADOR 2 SELECCIONARÁ ")
-    name = input("JUGADOR 1, INTRODUCE TU NOMBRE O ALIAS: ").lower()
-    name2 = input("JUGADOR 2, INTRODUCE TU NOMBRE O ALIAS: ").lower()
+    print(Fore.BLUE + "=" * 45 + "\n" + "\U0001F465 PARTIDA 2 JUGADORES.".center(40))
+    print(Fore.LIGHTWHITE_EX + "EL JUGADOR 1 ESCOGERÁ UN NÚMERO ENTRE 1 Y 1000, Y EL JUGADOR 2 DEBERÁ ADIVINARLO.")
+    name2 = input("JUGADOR 1, INTRODUCE TU NOMBRE: ").lower()
+    name = input("JUGADOR 2, INTRODUCE TU NOMBRE O ALIAS: ").lower()
     unknown_number = validation(int(input(Fore.LIGHTWHITE_EX + f"{name2}: INSERTA UN NÚMERO ENTRE 1 Y 1000: ")), 1, 1000)
-    play_game(max_attempts, unknown_number,name)
+    play_game(unknown_number,name)
+
 # FUNCION PARA GUARDAR ESTADISTICAS
 def save_statics(name,attempts,win,unknown_number,max_attempts):
     try:
@@ -127,9 +124,9 @@ def show_statics ():
         wb = openpyxl.load_workbook("GAME_STATICS.xlsx")
         Hoja = wb['statics']
         print(Fore.LIGHTCYAN_EX + "MENÚ".center(40))
-        print(Fore.RED + "NOTA: Las partidas aparecen guardadas en el archivo GAME_STATICS.xlsx")
+        print(Fore.RED + "NOTA: LAS PARTIDAS APARECEN GUARDADAS EN GAME_STATICS.xlsx")
         print(Fore.LIGHTCYAN_EX + "\t1. ESTADÍSTICAS GENERALES\n" + "\t2. ESTADÍSTICAS POR USUARIO\n" + Style.RESET_ALL)
-        option = int(input(Fore.BLUE + "SELECCIONE QUÉ DESEA HACER: "))
+        option = int(input(Fore.BLUE + "SELECCIONE UNA OPCIÓN: "))
         option = validation(option, 1, 2)
         if option == 1:
             print(Fore.LIGHTCYAN_EX + "\nESTADÍSTICAS GENERALES".center(45) + Style.RESET_ALL)
@@ -150,9 +147,9 @@ def show_statics ():
             wins = list(player_wins.values())
             plt.figure(figsize=(10, 6))
             plt.bar(names, wins, color='green')
-            plt.xlabel('Nombres de Jugadores')
-            plt.ylabel('Partidas Ganadas')
-            plt.title('Partidas Ganadas por Jugador')
+            plt.xlabel('JUGADORES')
+            plt.ylabel('PARTIDAS GANADAS')
+            plt.title('PARTIDAS GANADAS POR JUGADOR')
             plt.xticks(rotation=45, ha="right")
             plt.tight_layout()
             plt.show()
@@ -161,7 +158,7 @@ def show_statics ():
             total_losses = 0
             user_exists = False
             user = input('INTRODUZCA EL NOMBRE DEL USUARIO QUE DESEA BUSCAR: ')
-            print(Fore.GREEN + f"\nInformación del usuario {user}:" + Style.RESET_ALL)
+            print(Fore.GREEN + f"\nINFORMACIÓN DE {user}:" + Style.RESET_ALL)
             for row in Hoja.iter_rows(min_row=2, values_only=True):
                 name, win, unknown_number, attempts, max_attempts, date = row
                 if name.lower() == user.lower():
@@ -174,18 +171,18 @@ def show_statics ():
                     else:
                         total_losses += 1
             if user_exists == True:
-                categories = ['Partidas Ganadas', 'Partidas Perdidas']
+                categories = ['PARTIDAS GANADAS', 'PARTIDAS PERDIDAS']
                 values = [total_wins, total_losses]
                 plt.figure(figsize=(8, 6))
                 plt.bar(categories, values, color=['green', 'red'])
-                plt.xlabel('Categorías')
-                plt.ylabel('Cantidad de Partidas')
-                plt.title(f'Estadísticas de {user}')
+                plt.xlabel('CATEGORÍAS')
+                plt.ylabel('CANTIDAD DE PARTIDAS')
+                plt.title(f'ESTADÍSTICAS DE {user}')
                 plt.show()
             else:
-                print(f'EL USUARIO {user} NO EXISTE')
+                print(Fore.RED + f'EL USUARIO {user} NO EXISTE')
     except FileNotFoundError:
-        print("ERROR: ARCHIVO NO ENCONTRADO. ASEGÚRATE DE JUGAR AL MENOS UNA PARTIDA ANTES")
+        print("\u274C ERROR: ARCHIVO NO ENCONTRADO. ASEGÚRATE DE JUGAR AL MENOS UNA PARTIDA ANTES" + Style.RESET_ALL)
 
 # PROGRAMA PRINCIPAL
 selection = "0"
