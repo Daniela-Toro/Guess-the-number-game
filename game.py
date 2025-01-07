c288252de2058019899239727b249fe94c026add
Daniela <nani06994tr@gmail.com>
2025-01-07 20:36:08 +0100

    Refactor statistics functions

--- game.py
+++ game.py
@@ -26,7 +26,6 @@ def validation(value, min, max):
         value = int(input(Fore.LIGHTRED_EX + f"\U0001F534 TIENES QUE SELECCIONAR UNA OPCIÓN ENTRE {min} Y {max}: "))
     return value
 
-
 # FUNCION PARA MOSTRAR EL MENU PRINCIPAL
 def Menu():
     print(Fore.LIGHTBLUE_EX + "▓" * 50)
@@ -101,7 +100,7 @@ def play_game(unknown_number, name):
             Fore.RED + Style.BRIGHT + f"\U0001F534 {name} HAS SUPERADO EL NÚMERO MÁXIMO DE INTENTOS PERMITIDOS\nSUERTE PARA LA PRÓXIMA")
         print(Fore.RED + Style.BRIGHT + '\U0001F480 GAME OVER \u2620')
         animation_game('lose.jpg', 'game_over.wav')
-    save_statics(name, attempts, win, unknown_number, max_attempts)
+    save_statistics(name, attempts, win, unknown_number, max_attempts)
 
 
 # FUNCION PARA MODO SOLITARIO
@@ -126,90 +125,97 @@ def two_players():
 
 
 # FUNCION PARA GUARDAR ESTADISTICAS
-def save_statics(name, attempts, win, unknown_number, max_attempts):
+def save_statistics(name, attempts, win, unknown_number, max_attempts):
     try:
-        wb = openpyxl.load_workbook("GAME_STATICS.xlsx")
+        wb = openpyxl.load_workbook("GAME_STATISTICS.xlsx")
         sheet = wb.active
     except FileNotFoundError:
         wb = Workbook()
         sheet = wb.active
-        sheet.title = "statics"
+        sheet.title = "statistics"
         sheet.append(["NOMBRE", "GANADOR", "NÚMERO_SECRETO", "INTENTOS_UTILIZADOS", "INTENTOS_TOTALES", "FECHA"])
-        print(Fore.YELLOW + "\U0001F4C2 ARCHIVO GAME_STATICS.xlsx CREADO")
+        print(Fore.YELLOW + "\U0001F4C2 ARCHIVO GAME_STATISTICS.xlsx CREADO")
     dt = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     sheet.append([name, win, unknown_number, attempts, max_attempts, dt])
-    wb.save("GAME_STATICS.xlsx")
+    wb.save("GAME_STATISTICS.xlsx")
     print(Fore.YELLOW + "\U0001F4BE RESULTADO DE LA PARTIDA GUARDADO EN ESTADÍSTICAS")
 
 
 #FUNCION PARA MOSTRAR ESTADISTICAS
-def show_statics():
+def show_statistics():
     print(Fore.LIGHTBLUE_EX + "▓" * 50)
-    print(Fore.LIGHTBLUE_EX + "\U0001F4CA ESTADÍSTICAS.".center(40))
+    print(Fore.LIGHTBLUE_EX + "\U0001F4CA ESTADÍSTICAS".center(50))
     try:
-        wb = openpyxl.load_workbook("GAME_STATICS.xlsx")
-        Hoja = wb['statics']
+        wb = openpyxl.load_workbook("GAME_STATISTICS.xlsx")
+        Hoja = wb['statistics']
         print(Fore.LIGHTBLUE_EX + "MENÚ".center(50))
-        print(Fore.RED + "\u26A0 LAS PARTIDAS APARECEN GUARDADAS EN GAME_STATICS.xlsx")
+        print(Fore.RED + "\u26A0 LAS PARTIDAS APARECEN GUARDADAS EN GAME_STATISTICS.xlsx")
         print(Fore.LIGHTBLUE_EX + "\t1. ESTADÍSTICAS GENERALES\n" + "\t2. ESTADÍSTICAS POR USUARIO\n" + Style.RESET_ALL)
         option = int(input(Fore.LIGHTCYAN_EX + "\u27A1 SELECCIONE UNA OPCIÓN: "))
         option = validation(option, 1, 2)
         user = None
-        if option == 2:
-            user = names_validation(
-                input(Fore.LIGHTCYAN_EX + 'INTRODUZCA EL NOMBRE DEL USUARIO QUE DESEA BUSCAR: ').upper())
-        players = {}
-        show_header = False
-        for row in Hoja.iter_rows(min_row=2, values_only=True):
-            name, win, unknown_number, attempts, max_attempts, date = row
-            if option == 1 or (option == 2 and name == user):
-                if show_header == False:
-                    print(Fore.LIGHTBLUE_EX + "\nESTADÍSTICAS GENERALES".center(50) + Style.RESET_ALL)
-                    for cell in Hoja[1]:
-                        print(cell.value, end=" ")
-                    print()
-                    show_header = True
-                if name not in players:
-                    players[name] = {
-                        "wins": 0,
-                        "losses": 0
-                    }
-                if win == True:
-                    players[name]["wins"] += 1
-                else:
-                    players[name]["losses"] += 1
-                for cell in row:
-                    print(cell, end=" ")
-                print()
-        if option == 1:
-            names = list(players.keys())
-            wins = []
-            for name in names:
-                wins.append(players[name]["wins"])
-            plt.figure(figsize=(10, 6))
-            plt.bar(names, wins, color='green')
-            plt.xlabel('JUGADORES')
-            plt.ylabel('PARTIDAS GANADAS')
-            plt.title('PARTIDAS GANADAS POR JUGADOR')
-            plt.xticks(rotation=45, ha="right")
-            plt.tight_layout()
-            plt.show()
-        else:
-            if user in players:
-                categories = ['PARTIDAS GANADAS', 'PARTIDAS PERDIDAS']
-                values = [players[user]["wins"], players[user]["losses"]]
-                plt.figure(figsize=(8, 6))
-                plt.bar(categories, values, color=['green', 'red'])
-                plt.xlabel('CATEGORÍAS')
-                plt.ylabel('CANTIDAD DE PARTIDAS')
-                plt.title(f'ESTADÍSTICAS DE {user}')
-                plt.show()
-            else:
-                print(Fore.LIGHTRED_EX + f'EL USUARIO {user} NO EXISTE')
+        statistical_logic(Hoja, option, user)
     except FileNotFoundError:
-        print(
-            Fore.LIGHTRED_EX + "\u274C ERROR: ARCHIVO NO ENCONTRADO. ASEGÚRATE DE JUGAR AL MENOS UNA PARTIDA ANTES" + Style.RESET_ALL)
+        print(Fore.LIGHTRED_EX + "\u274C ERROR: ARCHIVO NO ENCONTRADO. ASEGÚRATE DE JUGAR AL MENOS UNA PARTIDA ANTES" + Style.RESET_ALL)
 
+def statistical_logic(Hoja, option, user):
+    if option == 2:
+        user = names_validation(input(Fore.LIGHTCYAN_EX + 'INTRODUZCA EL NOMBRE DEL USUARIO QUE DESEA BUSCAR: ').upper())
+    players = {}
+    show_header = False
+    for row in Hoja.iter_rows(min_row=2, values_only=True):
+        name, win, unknown_number, attempts, max_attempts, date = row
+        if option == 1 or (option == 2 and name == user):
+            if show_header == False:
+                print(Fore.LIGHTBLUE_EX + "\nESTADÍSTICAS GENERALES".center(50) + Style.RESET_ALL)
+                for cell in Hoja[1]:
+                    print(cell.value, end=" ")
+                print()
+                show_header = True
+            if name not in players:
+                players[name] = {
+                    "wins": 0,
+                    "losses": 0
+                }
+            if win == True:
+                players[name]["wins"] += 1
+            else:
+                players[name]["losses"] += 1
+            for cell in row:
+                print(cell, end=" ")
+            print()
+    if option == 1:
+        plot_general_statistics(players)
+
+    else:
+        plot_user_statistics(players, user)
+
+def plot_general_statistics (players):
+    names = list(players.keys())
+    wins = []
+    for name in names:
+        wins.append(players[name]["wins"])
+    plt.figure(figsize=(10, 6))
+    plt.bar(names, wins, color='green')
+    plt.xlabel('JUGADORES')
+    plt.ylabel('PARTIDAS GANADAS')
+    plt.title('PARTIDAS GANADAS POR JUGADOR')
+    plt.xticks(rotation=45, ha="right")
+    plt.tight_layout()
+    plt.show()
+
+def plot_user_statistics (players, user):
+    if user in players:
+        categories = ['PARTIDAS GANADAS', 'PARTIDAS PERDIDAS']
+        values = [players[user]["wins"], players[user]["losses"]]
+        plt.figure(figsize=(8, 6))
+        plt.bar(categories, values, color=['green', 'red'])
+        plt.xlabel('CATEGORÍAS')
+        plt.ylabel('CANTIDAD DE PARTIDAS')
+        plt.title(f'ESTADÍSTICAS DE {user}')
+        plt.show()
+    else:
+        print(Fore.LIGHTRED_EX + f'EL USUARIO {user} NO EXISTE')
 
 # PROGRAMA PRINCIPAL
 print(Fore.LIGHTBLUE_EX + "▓" * 50)
@@ -224,7 +230,7 @@ while selection != "4":
     elif selection == "2":
         two_players()
     elif selection == "3":
-        show_statics()
+        show_statistics()
     elif selection == "4":
         print(Fore.LIGHTRED_EX + "GRACIAS POR JUGAR, HASTA LA PRÓXIMA! \U0001F44B")
     else:
